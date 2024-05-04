0288b090e838e60cdbc6a021a3c3e88f5598947c
MS-vizja <166212512+MS-vizja@users.noreply.github.com>
2024-05-04 12:01:26 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import company.exceptions.InvalidAgeException;
 public class Main {
     public static void main(String[] args) {
         try {
-            Person person = new Person("Marek Mostowiak", 24);
+            Person person = new Person("Mariusz Pudzianowski", 24);
             System.out.println("Osoba: " + person.getName() + ", Wiek: " + person.getAge());
         } catch (InvalidAgeException e) {
             System.out.println("Błąd: " + e.getMessage());
